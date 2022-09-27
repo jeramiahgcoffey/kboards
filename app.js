01d890a09aa2e32a8db07a1f98ac2a235ebc7388
Jeramiah Coffey <jcoffey@rategenius.com>
2022-09-27 15:27:04 -0500

    Connect db

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 require('dotenv').config();
+const connectDB = require('./db/connect');
+
 const express = require('express');
 const app = express();
 
 
 const port = process.env.PORT || 3000;
-const start = () => {
-  app.listen(port, () => console.log(`Server listening on ${ port }...`));
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () => console.log(`Server listening on ${ port }...`));
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 start();
