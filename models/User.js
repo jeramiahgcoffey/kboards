9178c9d763a297df85d03c4883677c79d7ef311d
Jeramiah Coffey <jcoffey@rategenius.com>
2022-09-27 16:49:38 -0500

    User model

--- models/User.js
+++ models/User.js
@@ -1,1 +1,27 @@
 import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+const UserSchema = new Schema({
+  email: {
+    type: String,
+    required: [true, 'Email is required'],
+    minlength: [5, 'Please enter a valid email address'],
+    maxlength: [255, 'Please enter less than 255 characters'],
+    unique: true,
+    lowercase: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: [true, 'Password is required'],
+  },
+  name: {
+    type: String,
+    minlength: [3, 'Name must be at least 3 characters'],
+    maxlength: [30, 'Name must be at most 12 characters'],
+    trim: true,
+  },
+});
+
+const User = mongoose.model('User', UserSchema);
+export default User;
