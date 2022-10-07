3e7f5351a5808bce1ee790ac480cd3c4a3f201c8
Jeramiah Coffey <jcoffey@rategenius.com>
2022-10-07 10:45:53 -0500

    Install Quasar

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,9 +1,7 @@
-<script setup>
-import { RouterLink, RouterView } from "vue-router";
-</script>
-
 <template>
-  <RouterView />
+  <router-view />
 </template>
 
-<style scoped></style>
+<script setup lang="ts">
+
+</script>
