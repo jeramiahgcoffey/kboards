33a93c4c653947752caf38c61c5f531a8737b6c1
Jeramiah Coffey <jcoffey@rategenius.com>
2022-10-09 15:15:11 -0500

    remove front

--- client/src/common/axios.config.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const apiRequest = axios.create({
-  baseURL: "http://localhost:5001/api/v1",
-});
-
-const createAPIRequest = () => apiRequest;
-
-export { createAPIRequest };
