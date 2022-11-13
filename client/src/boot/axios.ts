eb52a164643ba1f8de243b81f282bfcc8c8bcd3c
Jeramiah Coffey <jcoffey@rategenius.com>
2022-11-12 20:35:23 -0600

    deploy

--- client/src/boot/axios.ts
+++ client/src/boot/axios.ts
@@ -15,7 +15,7 @@ declare module '@vue/runtime-core' {
 // "export default () => {}" function below (which runs individually
 // for each client)
 const api = axios.create({
-  baseURL: 'http://localhost:5001/api/v1',
+  baseURL: 'https://kboards.onrender.com/api/v1',
 });
 
 api.interceptors.request.use((config) => {
