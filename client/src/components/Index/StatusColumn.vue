8ca149cd8f047c4cb67c4f1f16d55ff9c0821577
Jeramiah <jeramiahcoffey.dev@gmail.com>
2023-01-25 10:01:50 -0600

    implement vuedraggable for moving tasks between columns

--- client/src/components/Index/StatusColumn.vue
+++ client/src/components/Index/StatusColumn.vue
@@ -27,19 +27,24 @@
         ></q-btn>
       </div>
     </div>
-    <div>
-      <task-card
-        v-for="task in store.tasksByColumn(column._id)"
-        :key="task._id"
-        :task="task"
-      />
-    </div>
+    <draggable
+      :model-value="store.tasksByColumn(props.column._id)"
+      @change="test"
+      group="tasks"
+      item-key="_id"
+    >
+      <template #item="{ element: task }">
+        <task-card :task="task" />
+      </template>
+    </draggable>
   </q-scroll-area>
 </template>
 
 <script setup>
 import TaskCard from 'src/components/Index/TaskCard.vue';
 import { useStore } from 'src/stores/store';
+import { computed } from '@vue/reactivity';
+import draggable from 'vuedraggable';
 
 const props = defineProps(['column']);
 
@@ -49,5 +54,25 @@ const openEditColumn = () => {
   store.dialogContent = 'editColumn';
   store.loadDraftColumn(props.column);
   store.dialogOpen = true;
+};
+
+const tasksByColumn = computed({
+  get() {
+    return store.tasksByColumn(props.column._id);
+  },
+  set(newValue) {
+    console.log(newValue);
+    return;
+  },
+});
+
+const test = (e) => {
+  if (e?.added?.element) {
+    store.loadDraftTask(e?.added.element);
+    store.draftTask.status = store.columns.find(
+      (c) => c._id === props.column._id
+    );
+    store.updateTask();
+  }
 };
 </script>
