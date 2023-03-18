fae4571bd24a18a9619251b6531465f37b0bcd48
Jeramiah Coffey <jerrygcoffey@gmail.com>
2023-03-18 10:32:09 -0700

    feat: implement error handling for duplicate keys

--- src/middleware/error-handler.ts
+++ src/middleware/error-handler.ts
@@ -1,6 +1,7 @@
 import { StatusCodes } from 'http-status-codes';
 import CustomAPIError from '../errors/custom-api.js';
 import { NextFunction, Request, Response } from 'express';
+import { MongoServerError } from 'mongodb';
 
 const errorHandlerMiddleware = (
   err: Error,
@@ -8,8 +9,16 @@ const errorHandlerMiddleware = (
   res: Response,
   next: NextFunction
 ) => {
+  console.log(err);
+  
+
   if (err instanceof CustomAPIError) {
     return res.status(err.statusCode).json({ message: err.message });
+  }
+
+  if ((err as MongoServerError)?.code === 11000) {
+    const field = Object.keys((err as MongoServerError).keyValue)[0]
+    return res.status(StatusCodes.CONFLICT).json({message: `That ${field} is already associated with an account.`})
   }
 
   const message = err.message || 'Internal Server Error';
